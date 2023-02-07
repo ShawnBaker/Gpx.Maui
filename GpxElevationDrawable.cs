2fd17c03d1b06e9de17903747018adc82f57ec02
shawn <shawn@frozen.ca>
2023-02-06 22:30:53 -0600

    Initial version.

---
+++ GpxElevationDrawable.cs
@@ -0,0 +1,237 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using FrozenNorth.Gpx;
+
+namespace FrozenNorth.Gpx.Maui
+{
+    public class GpxElevationDrawable : IDrawable
+    {
+        // instance variables
+        private DateTime time = DateTime.MinValue;
+        private double reductionTolerance = 0;
+        private GpxPointList originalPoints = null;
+		private GpxTrack originalTrack = null;
+		private GpxTrackSegmentList segments = new GpxTrackSegmentList();
+		private List<GpxPointList> points = new List<GpxPointList>();
+
+        /// <summary>
+        /// Space between the outer edge of the drawable and the graph itself.
+        /// </summary>
+        public int Padding { get; set; } = 10;
+
+		/// <summary>
+		/// Width of the lines in the graph.
+		/// </summary>
+		public int LineWidth { get; set; } = 2;
+
+		/// <summary>
+		/// Color of the lines being drawn.
+		/// </summary>
+		public Color LineColor { get; set; } = Color.FromRgb(255, 255, 0);
+
+		/// <summary>
+		/// Color of the position bar.
+		/// </summary>
+		public Color PositionBarColor { get; set; } = Color.FromRgb(255, 255, 0);
+
+		/// <summary>
+		/// Determines whether or not the position bar is displayed.
+		/// </summary>
+		public bool ShowPositionBar { get; set; } = false;
+
+        /// <summary>
+        /// Width of top and botton ends of the position bar.
+        /// </summary>
+        public int PositionBarEndWidth { get; set; } = 6;
+
+        /// <summary>
+        /// Minimum elevation range of the graph in meters.
+        /// </summary>
+		public int MinElevationRange { get; set; } = 0;
+
+        /// <summary>
+        /// True if there are some points to be drawn.
+        /// </summary>
+		public bool HasPoints => segments.HasPoints;
+
+		/// <summary>
+		/// Duration of the points to be drawn.
+		/// </summary>
+		public TimeSpan Duration => segments.Duration;
+
+		/// <summary>
+		/// Start time of the points to be drawn.
+		/// </summary>
+		public DateTime StartTime => segments.StartTime;
+
+		/// <summary>
+		/// End time of the points to be drawn.
+		/// </summary>
+		public DateTime EndTime => segments.EndTime;
+
+        /// <summary>
+        /// Tolerance to be used when reducing the number of points using the Douglas Peucker algorithm.
+        /// </summary>
+        public double ReductionTolerance
+        {
+            get => reductionTolerance;
+			set
+            {
+                double newTolerance = Math.Min(Math.Max(value, 0), 1);
+                if (newTolerance != reductionTolerance)
+                {
+                    reductionTolerance = newTolerance;
+					GetPoints();
+				}
+			}
+        }
+
+		/// <summary>
+		/// Sets the points to be drawn.
+		/// </summary>
+		public GpxPointList Points
+		{
+            get => originalPoints;
+            set
+            {
+                originalPoints = value;
+                originalTrack = null;
+                segments.Clear();
+                if (originalPoints != null)
+                {
+                    var segment = new GpxTrackSegment();
+                    segment.Points.AddRange(originalPoints);
+                    segments.Add(segment);
+                }
+                GetPoints();
+            }
+		}
+
+		/// <summary>
+		/// Sets the points to be drawn from a track.
+		/// </summary>
+		public GpxTrack Track
+        {
+            get => originalTrack;
+            set
+            {
+				originalTrack = value;
+				originalPoints = null;
+				segments.Clear();
+                if (originalTrack != null)
+                {
+                    foreach (var segment in originalTrack.Segments)
+                    {
+                        segments.Add(segment);
+                    }
+                }
+                GetPoints();
+            }
+		}
+
+		/// <summary>
+		/// Gets/sets the current time (i.e where the position bar will be drawn).
+		/// </summary>
+		public DateTime Time
+        {
+            get => time;
+            set
+            {
+                time = value;
+                if (time < StartTime)
+                    time = StartTime;
+                else if (time > EndTime)
+                    time = EndTime;
+            }
+        }
+
+        /// <summary>
+        /// Gets the points to be drawn, all or reduced.
+        /// </summary>
+        private void GetPoints()
+        {
+			points.Clear();
+            foreach (var segment in segments)
+            {
+                if (reductionTolerance != 0)
+                {
+                    points.Add(segment.Points.GetReducedElevationPoints(reductionTolerance));
+                }
+                else
+                {
+                    points.Add(segment.Points);
+                }
+            }
+			Time = StartTime;
+		}
+
+		/// <summary>
+		/// Draws the graph and position bar.
+		/// </summary>
+		/// <param name="canvas">Canvas to draw on.</param>
+		/// <param name="dirtyRect">Rectangle to draw in.</param>
+		public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            // configure the canvas
+            canvas.StrokeSize = LineWidth;
+            canvas.StrokeColor = LineColor;
+
+            // draw the graph
+            double w = dirtyRect.Width - Padding * 2;
+            double h = dirtyRect.Height - Padding * 2;
+            if (HasPoints)
+            {
+                foreach (var segment in points)
+                {
+                    double x, y;
+                    double totalSeconds = segment.Duration.TotalSeconds;
+					double elevationRange = segment.GetElevationRange(out double low, out double high);
+                    if (elevationRange <= 0) elevationRange = 0;
+					if (elevationRange < MinElevationRange)
+                    {
+						low -= (MinElevationRange - elevationRange) / 2;
+                        elevationRange = MinElevationRange;
+					}
+					DateTime startTime = (segment[0].Time != null) ? segment[0].Time.Value : DateTime.MinValue;
+
+                    double elevation = (segment[0].Elevation != null) ? segment[0].Elevation.Value : 0;
+					y = (elevation - low) / elevationRange;
+                    PointF prev = new PointF(Padding, (float)(Padding + y * h));
+                    for (int i = 1; i < segment.Count; i++)
+                    {
+						DateTime time = (segment[i].Time != null) ? segment[i].Time.Value : DateTime.MinValue;
+						x = (time - startTime).TotalSeconds / totalSeconds;
+						elevation = (segment[i].Elevation != null) ? segment[i].Elevation.Value : 0;
+						y = 1 - (elevation - low) / elevationRange;
+                        PointF next = new PointF((float)(Padding + x * w), (float)(Padding + y * h));
+                        canvas.DrawLine(prev, next);
+                        prev = next;
+                    }
+                }
+            }
+            else
+            {
+                LineGeometry line = new LineGeometry();
+                double y = Padding + h / 2.0;
+                canvas.DrawLine(Padding, (float)y, (float)(Padding + w), (float)y);
+            }
+
+            // draw the position bar
+            if (ShowPositionBar)
+            {
+                TimeSpan barOffset = Time - StartTime;
+                float ox = Padding;
+                if (HasPoints)
+                {
+                    ox += (float)barOffset.Ticks / Duration.Ticks * (float)w;
+                }
+				canvas.StrokeColor = PositionBarColor;
+				canvas.DrawLine(ox, Padding, ox, (float)(Padding + h));
+                float oy = Padding;
+                float half = PositionBarEndWidth / 2;
+                canvas.DrawLine(ox - half, oy, ox + half, oy);
+                oy += (float)h;
+                canvas.DrawLine(ox - half, oy, ox + half, oy);
+            }
+        }
+    }
+}
---
+++ GpxElevationGraph.cs
@@ -0,0 +1,279 @@
+﻿using FrozenNorth.Gpx;
+
+namespace FrozenNorth.Gpx.Maui
+{
+	public class GpxElevationGraph : GraphicsView
+	{
+		// instance variables
+		private GpxElevationDrawable drawable = new GpxElevationDrawable();
+		private bool userCanMovePositionBar = false;
+
+		/// <summary>
+		/// If ShowPositionBar is true, fired when the user interactively moves the position bar.
+		/// </summary>
+		public EventHandler<TouchEventArgs> PositionBarMoved;
+
+		/// <summary>
+		/// Creates a GraphicsView with a GpxTrackElevationDrawable drawable.
+		/// </summary>
+		public GpxElevationGraph()
+			: base()
+		{
+			Drawable = drawable;
+		}
+
+		/// <summary>
+		/// Space between the outer edge of the drawable and the graph itself.
+		/// </summary>
+		public int Padding
+		{
+			get => drawable.Padding;
+			set
+			{
+				if (value != drawable.Padding)
+				{
+					drawable.Padding = value;
+					Invalidate();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Width of the lines in the graph.
+		/// </summary>
+		public int LineWidth
+		{
+			get => drawable.LineWidth;
+			set
+			{
+				if (value != drawable.LineWidth)
+				{
+					drawable.LineWidth = value;
+					Invalidate();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Color of the lines being drawn.
+		/// </summary>
+		public Color LineColor
+		{
+			get => drawable.LineColor;
+			set
+			{
+				if (value != drawable.LineColor)
+				{
+					drawable.LineColor = value;
+					Invalidate();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Color of the position bar.
+		/// </summary>
+		public Color PositionBarColor
+		{
+			get => drawable.PositionBarColor;
+			set
+			{
+				if (value != drawable.PositionBarColor)
+				{
+					drawable.PositionBarColor = value;
+					Invalidate();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Determines whether or not the position bar is displayed.
+		/// </summary>
+		public bool ShowPositionBar
+		{
+			get => drawable.ShowPositionBar;
+			set
+			{
+				if (value != drawable.ShowPositionBar)
+				{
+					drawable.ShowPositionBar = value;
+					Invalidate();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Width of top and botton ends of the position bar.
+		/// </summary>
+		public int PositionBarEndWidth
+		{
+			get => drawable.PositionBarEndWidth;
+			set
+			{
+				if (value != drawable.PositionBarEndWidth)
+				{
+					drawable.PositionBarEndWidth = value;
+					Invalidate();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Minimum elevation range of the graph in meters.
+		/// </summary>
+		public int MinElevationRange
+		{
+			get => drawable.MinElevationRange;
+			set
+			{
+				if (value != drawable.MinElevationRange)
+				{
+					drawable.MinElevationRange = value;
+					Invalidate();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Sets the points to be drawn.
+		/// </summary>
+		public GpxPointList Points
+		{
+			get => drawable.Points;
+			set
+			{
+				if (value != drawable.Points)
+				{
+					drawable.Points = value;
+					Invalidate();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Sets the points to be drawn from a track.
+		/// </summary>
+		public GpxTrack Track
+		{
+			get => drawable.Track;
+			set
+			{
+				if (value != drawable.Track)
+				{
+					drawable.Track = value;
+					Invalidate();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets/sets the current time (i.e where the bar will be drawn).
+		/// </summary>
+		public DateTime Time
+		{
+			get => drawable.Time;
+			set
+			{
+				if (value != drawable.Time)
+				{
+					drawable.Time = value;
+					Invalidate();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Duration of the track.
+		/// </summary>
+		public TimeSpan Duration => drawable.Duration;
+
+		/// <summary>
+		/// Start time of the track.
+		/// </summary>
+		public DateTime StartTime => drawable.StartTime;
+
+		/// <summary>
+		/// End time of the track.
+		/// </summary>
+		public DateTime EndTime => drawable.EndTime;
+
+		/// <summary>
+		/// Tolerance to be used when reducing the number of points using the Douglas Peucker algorithm.
+		/// </summary>
+		public double ReductionTolerance
+		{
+			get => drawable.ReductionTolerance;
+			set
+			{
+				if (value != drawable.ReductionTolerance)
+				{
+					drawable.ReductionTolerance = value;
+					Invalidate();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Enables/disables allowing the user to move the position bar.
+		/// </summary>
+		public bool UserCanMovePositionBar
+		{
+			get => userCanMovePositionBar;
+			set
+			{
+				if (value != userCanMovePositionBar)
+				{
+					userCanMovePositionBar = value;
+					if (userCanMovePositionBar)
+					{
+						StartInteraction += OnStartInteraction;
+						DragInteraction += OnDragInteraction;
+					}
+					else
+					{
+						StartInteraction -= OnStartInteraction;
+						DragInteraction -= OnDragInteraction;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Moves the time bar.
+		/// </summary>
+		private void OnStartInteraction(object sender, TouchEventArgs e)
+		{
+			SetTimeBarPosition(e);
+		}
+
+		/// <summary>
+		/// Moves the time bar.
+		/// </summary>
+		private void OnDragInteraction(object sender, TouchEventArgs e)
+		{
+			SetTimeBarPosition(e);
+		}
+
+		/// <summary>
+		/// Sets the time bar and video position from a mouse position.
+		/// </summary>
+		/// <param name="e">Mouse event arguments.</param>
+		private void SetTimeBarPosition(TouchEventArgs e)
+		{
+			if (drawable.HasPoints)
+			{
+				double w = Width - Padding * 2;
+				double x = e.Touches[0].X - Padding;
+				if (x < 0) x = 0;
+				if (x > w) x = w;
+				long position = (long)(Duration.Ticks * x / w);
+				DateTime newTime = StartTime + TimeSpan.FromTicks(position);
+				if (newTime != Time)
+				{
+					Time = newTime;
+					PositionBarMoved?.Invoke(this, e);
+				}
+			}
+		}
+	}
+}
